cf719bbd776867229e548ff28ba7a6a5b3a95f0b
Asad-the-One <asadsyedhussain@hotmail.com>
2026-02-12 00:10:28 +0000

    Implemented pathfinding

--- core_algorithms.py
+++ core_algorithms.py
@@ -13,6 +13,7 @@
 
 # Module imports
 import random
+import math
 
 # Function beginning
 def generate_maze(num_cols, num_rows, knock_prob=0.1):
@@ -99,7 +100,6 @@ def generate_maze(num_cols, num_rows, knock_prob=0.1):
                 # If there are 3 or 4 walls surrounding it on 4 sides, don't break the wall.
                 if walls <= 2 and random.random() <= knock_prob:
                     maze[row][col] = 0
-
     # Polish the maze.
 
     # Make borders one cell thick rather than 2.
@@ -136,3 +136,102 @@ def generate_maze(num_cols, num_rows, knock_prob=0.1):
 
 
     return maze
+
+# ##################### 
+
+# ---------------
+# A* Pathfinding Algorithm
+# ---------------
+
+# Requires cell class for managing and returning path
+
+class Cell:
+
+    # Initialise object
+    def __init__(self, pos, parent_coords, wall, f, g, h):
+
+        self.pos = pos
+        self.parent_coords = parent_coords
+        self.wall = wall
+        self.f = f
+        self.g = g
+        self.h = h
+        
+
+
+# A* Algorithm
+def astar(maze, start, end):
+
+    # Initialise start cell
+    start_cell = Cell(start, start, False, 0, 0, 0)
+
+    # Initialise open and closed lists
+    open = [start_cell]
+    closed = []
+
+    # Pathfinding. Loops until end reached.
+    found = False
+    while len(open) != 0 and not found:
+
+        # Sort open list in ascending order by f
+        open.sort(key = lambda x: x.f)
+        current_node = open[0]
+
+        # Move the current node to the closed list
+        closed.append(current_node)
+        open.remove(current_node)
+
+        # Generate successors for the node
+        successors = [
+            Cell([current_node.pos[0]-1, current_node.pos[1]], current_node.pos, False, 0, 0, 0), # Left
+            Cell([current_node.pos[0]+1, current_node.pos[1]], current_node.pos, False, 0, 0, 0), # Right
+            Cell([current_node.pos[0], current_node.pos[1]-1], current_node.pos, False, 0, 0, 0), # Up
+            Cell([current_node.pos[0], current_node.pos[1]+1], current_node.pos, False, 0, 0, 0) # Down
+        ]
+
+        # Define maze integer limits
+        row_limits = list(maze.keys())
+        col_limits = list(maze["0"].keys())
+        # Iterate through successors for destination check
+        for s in successors:
+
+            # DEBUGGING
+            #print(maze)
+            #print(maze["0"])
+            print(len(closed))
+
+            # Exit if s is the location
+            if s.pos == end:
+                found = True
+                open.append(s)
+                break
+            
+            # Otherwise, consider path cells only. First, check if the cell is within the maze bounds
+            elif str(int(s.pos[1])) in row_limits and str(int(s.pos[0])) in col_limits:
+                if not maze[str(int(s.pos[1]))][str(int(s.pos[0]))]["wall"]:
+
+                    # Compute g
+                    s.g = current_node.g + 1
+
+                    # Compute h using Euclidean heuristic (Pythagoras)
+                    dist_x = s.pos[0] - end[0]
+                    dist_y = s.pos[1] - end[1]
+                    s.h = math.sqrt(dist_x**2 + dist_y**2)
+
+                    # Compute f (no randomness yet)
+                    s.f = s.g + s.h # + random int
+
+                    # Skip this successor if there is an identical cell in the open or closed lists with a better f
+                    skip = False
+                    for i in open:
+                        if i.pos[0] == s.pos[0] and i.pos[1] == s.pos[1] and i.f <= s.f:
+                            skip = True
+                    if not skip:
+                        for i in closed:
+                            if i.pos[0] == s.pos[0] and i.pos[1] == s.pos[1] and i.f <= s.f:
+                                skip = True
+                    if not skip:
+                        open.append(s)
+    
+    # Return closed list to trace back path
+    return closed
--- main.py
+++ main.py
@@ -7,12 +7,11 @@ import core_classes as cc
 
 #maze1 = cc.Maze()
 #maze1.load_maze("maze3")
+#game1 = cc.Game(maze1, 100, window_width=1920, window_height=1080)
+#game1.run(0.05)
 #maze1.generate_maze("maze3", 100, 50)
 #print("Rendering...")
 #maze1.terminal_output(True)
-
-#game1 = cc.Game(maze1, 100, window_width=1920, window_height=1080)
-#game1.run(0.05)
 
 menu = cc.Menu()
 menu.main_menu()
